2ebc576f7e9fede423de777fe381c4fd02fca861
John Baumbach <john.j.baumbach@gmail.com>
2013-06-10 20:59:54 -0700

    Updates for site

--- common/globalfunctions.js
+++ common/globalfunctions.js
@@ -44,9 +44,6 @@ exports.getSessionInfo = function(req) {
 exports.setSessionInfo = function(req, sessionInfo) {
   try {
     req.session.sessionInfo = sessionInfo;
-    
-    console.log('(info) setting sessionInfo: ' + util.inspect(req.session.sessionInfo));
-    
   } catch(err) {
     throw 'Error setting session info - be sure redis is running: ' + err;    
   }
